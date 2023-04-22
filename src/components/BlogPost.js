0a37342d854c5cbbcefa37427cb0627889ae89e1
rukia <ruqayashar1@gmail.com>
2023-04-22 05:23:41 +0300

    complete

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -9,6 +9,8 @@ function BlogPost() {
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      
+      minutesToRead={1}
     </div>
   );
 }
